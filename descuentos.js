0c1b681874608d6edeb65b72a4048778ac5b1257
Pablo <guinazuzu@gmail.com>
2022-03-14 01:56:09 -0300

    Se crea text input y un boton que llama a la funcion de descuento y se crea un <p> que a parece el total descontado

--- descuentos.js
+++ descuentos.js
@@ -3,7 +3,21 @@
 function calcularPrecioConDescuento(Precio, Descuento){
 const porcentajePrecioConDescuento =100 - Descuento;
 const precioConDescuento =(Precio * porcentajePrecioConDescuento)/100;
+
 return precioConDescuento;
+}
+function BotonDescuento(){
+const inputPrice =document.getElementById("InputPrice");
+const priceValue = inputPrice.value;
+
+const inputDiscount =document.getElementById("InputDiscount");
+const DiscountValue = inputDiscount.value;
+const PrecioConDescuento =calcularPrecioConDescuento(priceValue, DiscountValue);
+
+const ResultPrice = document.getElementById("ResultPrice");
+ResultPrice.innerText = "Tu Precio con descuento son: $" + PrecioConDescuento;
+
+
 }
 
 // console.log({
