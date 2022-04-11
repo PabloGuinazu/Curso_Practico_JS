1363738b6da2a1ab049207242be905e068c56e29
Pablo <guinazuzu@gmail.com>
2022-04-11 01:10:07 -0400

    Se termina con el code de moda.js se hace funcion  que nuestra el elemento mas repetido

--- moda.js
+++ moda.js
@@ -17,10 +17,18 @@ const lista1Count ={};
 lista1.map(
     function(elemento){
         if(lista1Count[elemento]){
-            lista1Count[elemento]  +=1;
+            lista1Count[elemento]  +=1; //suma la cantidad de veces que se repite
         }else{
-        lista1Count[elemento]= 1;
+        lista1Count[elemento]= 1;// lo agrega si es la 1ra vez que aparece 
 
         }
     }
 );
+
+    const lista1Array = Object.entries(lista1Count).sort( //el array se convierte en objeto
+        function(elementoA, elementoB){
+            return elementoA[1] - elementoB[1]; 
+        }
+    );
+    
+    const moda = lista1Array[lista1Array.length - 1];
